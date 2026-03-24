3e0f45b7de0bba43d172dfe36a11d2dcb14d1d3c
joaquimcascao <joaquimmmcascao@gmail.com>
2026-03-24 17:27:19 -0300

    Create variants to AuthButton

--- frontend/src/components/AuthButton.jsx
+++ frontend/src/components/AuthButton.jsx
@@ -1,10 +1,17 @@
-export const AuthButton = ({ children, onClick }) => {
-  return (
-    <button 
-      onClick={onClick}
-      className="cursor-pointer bg-red-600 text-zinc-50 p-2 rounded-md hover:bg-red-700 transition-colors"
-    >
-      {children}
-    </button>
-  );
+export const AuthButton = ({ children, onClick, variant = "primary", className = "" }) => {
+	const base = "cursor-pointer transition-colors";
+
+	const variants = {
+		primary: "bg-red-600 text-zinc-50 hover:bg-red-700 p-2 rounded-md",
+		minimal: "bg-transparent text-zinc-400 hover:text-zinc-100 px-3"
+	};
+
+	return (
+		<button
+			onClick={onClick}
+			className={`${base} ${variants[variant]} ${className}`}
+		>
+			{children}
+		</button>
+	);
 };
--- frontend/src/components/Pokedex.jsx
+++ frontend/src/components/Pokedex.jsx
@@ -1,20 +1,7 @@
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { AuthButton } from "./AuthButton";
 
 export const PokedexHomepage = () => {
-	const [pokemonSearch, setPokemonSearch] = useState("");
-
-	const handleSearch = async () => {
-		if (!pokemonSearch.trim()) return;
-		console.log("Sending to backend!", pokemonSearch.toLowerCase());
-	}
-
-	const handleKeyDown = (e) => {
-		if (e.key === 'Enter') {
-			handleSearch();
-		}
-	};
-
 	return (
 		<div className="relative flex justify-center items-center min-h-screen bg-zinc-900">
 			<div className="absolute top-6 right-8 text-zinc-100 hover:text-zinc-400 transition-colors font-medium">
@@ -28,11 +15,11 @@ export const PokedexHomepage = () => {
 				<div className=" group flex bg-zinc-800 rounded-4xl items-center pl-3 hover:text-zinc-900 transition-all duration-300">
 					<Search className="text-zinc-100" />
 					<input type="text"
-						placeholder="Type a pokemon's name" className="text-zinc-100 py-2 px-4 w-64 outline-0" value={pokemonSearch}
-						onChange={(e) => setPokemonSearch(e.target.value)}
-						onKeyDown={handleKeyDown} />
+						placeholder="Type a pokemon's name" className="text-zinc-100 py-2 px-4 w-64 outline-0" />
+					<AuthButton variant="minimal" onClick={onclick}>
+						Search
+					</AuthButton>
 				</div>
-				<p className="text-zinc-500 text-sm mt-2">Press Enter to search</p>
 			</div>
 		</div>
 	)
